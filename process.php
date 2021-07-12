79b80984559f03bd434dab55bb564d376be1fb28
ndthinh.sgt <ndthinh.sgt@gmail.com>
2021-07-12 15:55:57 +0700

    #2 update new

--- process.php
+++ process.php
@@ -1,5 +1,10 @@
 <?php
+<<<<<<< HEAD
 echo 'fix process 1';
 echo 'fix process 2';
+=======
+echo 'process 1';
+echo 'process 2';
+>>>>>>> b6f0c08... Coding Process
 echo 'process 3';
 echo 'process 4';
